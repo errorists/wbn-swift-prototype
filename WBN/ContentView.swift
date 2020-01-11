08425cdf5516281ae470348a85a0e89590dab09c
Maciej Zadykowicz <hi@maciej.co>
2020-01-11 18:39:27 +0100

    Initial Commit

---
+++ WBN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WBN
+//
+//  Created by Maciej Zadykowicz on 11/01/2020.
+//  Copyright © 2020 Maciej Zadykowicz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
